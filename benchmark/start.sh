3ccb0f75fffa1c91e2389f28a44db960667f9663
shengliangd <shengliangd@outlook.com>
2020-11-05 13:57:57 +0800

    add log.txt

--- benchmark/start.sh
+++ benchmark/start.sh
@@ -7,34 +7,34 @@
 # Distributed Training Metrics Generation --2 Processes --Testing CPU increase effect & Arch Difference #
 
 # Feedforward Neural Net
+timeout=100.0
+for ((i=1;i<=7;i++))
+do
+    timeout=`echo "scale=5; $timeout/10.0" | bc`
+    for (( j = 1; j <=3;j++ ))
+    do  
+        for ((k=1;k<=20;k++))
+        do
+            for ((m=1;m<=20;m++))
+            do
+                python3 main.py --epochs=$j --workers=$k --arch=ff --timeout=$timeout
+            done
+        done 
+    done
+done
 
+# Convolutional Neural Net
 #for (( i = 1; i < 6; i++ ))
 #do
 #    for (( j = 2; j <=20;j++ ))
 #    do  
 #        for ((k=1;k<10;k++))
 #        do
-#            python3 src/main.py --epochs=$i --nodes=1 --procs=$j --arch=ff --order=y
+#            python3 src/main.py --epochs=$i --nodes=1 --procs=$j --arch=conv --order=y
 #        done 
 #        for ((k=1;k<10;k++))
 #        do
-#           python3 src/main.py --epochs=$i --nodes=1 --procs=$j --arch=ff --order=n
+#           python3 src/main.py --epochs=$i --nodes=1 --procs=$j --arch=conv --order=n
 #        done
 #    done
 #done
-
-# Convolutional Neural Net
-for (( i = 1; i < 6; i++ ))
-do
-    for (( j = 2; j <=20;j++ ))
-    do  
-        for ((k=1;k<10;k++))
-        do
-            python3 src/main.py --epochs=$i --nodes=1 --procs=$j --arch=conv --order=y
-        done 
-        for ((k=1;k<10;k++))
-        do
-           python3 src/main.py --epochs=$i --nodes=1 --procs=$j --arch=conv --order=n
-        done
-    done
-done
